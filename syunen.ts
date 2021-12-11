9d1364b68ae6781522e20b49457b957d1a339cd3
jigsaw <m@jgs.me>
2021-12-11 21:22:10 +0900

    Hey syuunen

--- syunen.ts
+++ syunen.ts
@@ -1,23 +1,20 @@
-import { zpad } from "./zpad.ts";
-
-const year = "2022";
-const start = new Date(`${year}/01/01`);
+const start = new Date("2020/01/01");
 let res = "";
+
+const zpad = (n: number) => n.toString().length === 1 ? `0${n}` : n;
 
 const main = (d: Date) => {
   const next = new Date(d.getTime() + 86400000);
-  const pm = d.getMonth();
   const cm = next.getMonth();
-  if (pm !== cm) res += `\n[*** [${year}${zpad(cm + 1)}]]\n`;
   const cd = next.getDate();
-  res += `#${year}${zpad(cm + 1)}${zpad(cd)}\n`;
+  res += `#${zpad(cm + 1)}${zpad(cd)}\n`;
   const py = d.getFullYear();
   const cy = next.getFullYear();
   if (py === cy) {
     main(next);
   } else {
     const tmp = res.split("\n");
-    console.log(tmp.slice(-3).join("\n") + tmp.slice(0, -3).join("\n"));
+    console.log(tmp.slice(-2).join("\n") + tmp.slice(0, -2).join("\n"));
   }
 };
 
---
+++ zpad.ts
@@ -0,0 +1,1 @@
+export const zpad = (n: number) => n.toString().length === 1 ? `0${n}` : n
