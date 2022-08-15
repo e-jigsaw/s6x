a8ef0c9bb4e3cd5a542916cbe8daa1afd3ce97ac
jigsaw <m@jgs.me>
2022-08-15 09:36:43 +0900

    Add timeformat

--- src/timestamp.ts
+++ src/timestamp.ts
@@ -2,5 +2,6 @@ import type { Scrapbox } from "scrapbox";
 
 declare const scrapbox: Scrapbox;
 
+scrapbox.TimeStamp.addFormat("#YYYYMMDD #MMDD");
 scrapbox.TimeStamp.addFormat("#YYYYMMDD");
 scrapbox.TimeStamp.addFormat("#MMDD");
