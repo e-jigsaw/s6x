00178b76b702cc35b0057646354eb550c7a5d2e7
jigsaw <m@jgs.me>
2022-09-23 15:02:35 +0900

    sync image

--- tasks/sync.ts
+++ tasks/sync.ts
@@ -13,14 +13,16 @@ type RawPage = {
   title: string;
   created: number;
   updated: number;
+  image: string | null;
 };
 
 const { data, error } = await supabase.from("pages").upsert(
-  (pub as RawPage[]).map(({ id, title, created, updated }) => ({
+  (pub as RawPage[]).map(({ id, title, created, updated, image }) => ({
     id,
     title,
     created: new Date(created * 1000),
     updated: new Date(updated * 1000),
+    image,
   })),
 );
 
