25487fd0b28631918bc580adce07a90a41307257
jigsaw <m@jgs.me>
2022-09-25 22:30:10 +0900

    use onclick

--- src/dailyNoteButton.ts
+++ src/dailyNoteButton.ts
@@ -10,15 +10,22 @@ scrapbox.on("project:changed", () => {
 });
 
 function setup() {
-  const a = document.createElement("a");
-  a.href = `/jgs/${format(new Date(), "yyyyMMdd")}`;
-  a.innerHTML = "<button>Daily Note</button>";
-  a.style.marginTop = "8px";
-  a.style.marginLeft = "8px";
-  document.querySelector(".global-menu-button")?.appendChild(a);
+  const button = document.createElement("button");
+  button.id = "__S6X__DAILYNOTE";
+  button.innerText = "Daily Note";
+  button.style.marginTop = "8px";
+  button.style.marginLeft = "8px";
+  button.onclick = () => {
+    location.href = `/jgs/${format(new Date(), "yyyyMMdd")}`;
+  };
+  document.querySelector(".global-menu-button")?.appendChild(button);
 }
 function run() {
 }
 function cleanup() {
+  const el = document.getElementById("__S6X__DAILYNOTE");
+  if (el) {
+    el.remove();
+  }
 }
 setup();
